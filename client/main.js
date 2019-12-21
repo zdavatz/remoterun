85a8f3a79ff19540afd69470c08c832aceac6638
hamzamu <dr.hamzamusa@gmail.com>
2019-12-21 20:27:30 +0300

    YAML setting and UI #1

--- client/main.js
+++ client/main.js
@@ -5,19 +5,54 @@ import './main.html';
 
 log = console.log
 
+
+/**
+ * 
+ */
+
 Meteor.startup(function(){
   Meteor.call('getSetting',null,(err,data)=>{
     log(err,data)
     if(data){
       App.setSetting(data)
-    }
-    
+    } 
   })
+
+  /**
+   * 
+   */
+
+
+})
+/**
+ * 
+ */
+Template.remoteFiles.onCreated(function(){
+  var logs = []
+  logs.push({title:'',msg:'The script x run successfully'})
+  App.setSetting({logs:logs})
 })
 
+/**
+ * 
+ */
 Template.remoteFiles.helpers({
   files(){
     return App.getSetting('files')
   }
 })
 
+Template.remoteFiles.events({
+  'click .runCommand':(e)=>{
+    e.preventDefault();
+    var command = $(e.currentTarget).attr('data-val')
+
+    log('command',command)
+
+    Meteor.call('runCommand' , command , (err, result)=>{
+      if(result){
+        log('result',result)
+      }
+    })
+  }
+})
--- server/main.js
+++ server/main.js
@@ -1,28 +1,67 @@
 /**
  * 
  */
-
 import YAML from 'yaml'
+import _ from 'lodash'
 import {
   Meteor
 } from 'meteor/meteor';
-
-
+const {
+  exec
+} = require('child_process');
+const { spawnSync } = require( 'child_process' );
 log = console.log
 Meteor.startup(() => {
   // code to run on server at startup
 });
-
-
-
+const file = Assets.getText('settings.yaml')
+const settings = YAML.parse(file)
 /**
  * 
  */
 Meteor.methods({
   getSetting() {
-    var settingFile = Assets.getText('settings.yaml')
-    var setting = YAML.parse(settingFile)
-    log('Loading settings: --- ',setting)
-    return setting
+    log('Loading settings: --- ', settings)
+    /**
+     * 
+     */
+    var files = settings.files;
+    var files = _.map(files,(file)=>{
+      return {name:file.name, id: file.id}
+    })
+    settings.files = files
+    log('files',files)
+    return settings
+  },
+  /**
+   * 
+   * @param {*} command 
+   */
+  runCommand(command) {
+    /**
+     * Get the command based on the ID
+     */
+    var file = _.find(settings.files, (i) => {
+      return i.id = command
+    })
+
+    if(!file){
+      throw new Meteor.Error('command-err','Command does not exist')
+      return
+    }
+    log('file',file.file)
+    var command = file.file;
+    var result = {}
+    /**
+     * Running a command with paramters
+     * const child = spawnSync('ls', ['-lh', '/usr']);
+     */
+    const commandRun = spawnSync( command );
+    result.log = commandRun.stdout.toString()
+    result.err = commandRun.stderr.toString()
+    return result
   }
 })
+/**
+ * 
+ */
