9912dbf883d49bb65ca818df37cda2c4ecc0b7ac
hamzamu <dr.hamzamusa@gmail.com>
2019-12-26 21:27:54 +0300

    pty.js added

--- client/main.js
+++ client/main.js
@@ -1,15 +1,21 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-
 import './main.html';
 
-log = console.log
+const stripAnsi = require('strip-ansi');
 
 
 /**
  * 
  */
-
+Logs = new Mongo.Collection('logs')
+/**
+ * 
+ */
+log = console.log
+/**
+ * 
+ */
 Meteor.startup(function(){
   Meteor.call('getSetting',null,(err,data)=>{
     log(err,data)
@@ -17,12 +23,9 @@ Meteor.startup(function(){
       App.setSetting(data)
     } 
   })
-
   /**
    * 
    */
-
-
 })
 /**
  * 
@@ -32,28 +35,63 @@ Template.remoteFiles.onCreated(function(){
   logs.push({title:'',msg:'The script x run successfully'})
   App.setSetting({logs:[]})
 })
-
 /**
  * 
  */
 Template.remoteFiles.helpers({
   files(){
     return App.getSetting('files')
-  }
-})
+  },
 
+})
 Template.remoteFiles.events({
   'click .runCommand':(e)=>{
     e.preventDefault();
     var command = $(e.currentTarget).attr('data-val')
-
     log('command',command)
-
     Meteor.call('runCommand' , command , (err, result)=>{
       if(result){
         log('result',result)
         App.pushSetting({logs:result})
       }
     })
+  }
+})
+
+/**
+ * 
+ */
+
+ Template.logs.onRendered(function(){
+  // var term = new Terminal();
+  // term.open(document.getElementById('terminal'));
+
+  // term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
+ })
+ /**
+  * 
+  */
+Template.logs.helpers({
+
+  logs(){
+    return Logs.find({},{sort: {createdAt: -1},limit:20})
+  },
+  logX(){
+     var logX = Logs.findOne()
+     log(logX)
+    //  var logX = JSON.parse(logX)
+    // term.write(logX.log)
+    if(logX){
+      return logX.log
+    }
+  },
+  getLog(data){
+    if(!data){
+      return
+    }
+    //var data = data.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, " ")
+    var data = data.replace(/otify;/g, "")
+    var data = data.replace(/777;preexec/g, "")
+    return stripAnsi(data)
   }
 })
--- server/main.js
+++ server/main.js
@@ -6,6 +6,10 @@ import _ from 'lodash'
 import {
   Meteor
 } from 'meteor/meteor';
+/** */
+const pty = require('pty.js');
+const stripAnsi = require('strip-ansi');
+/** */
 var exec = require('child_process').exec;
 const {
   execSync
@@ -16,6 +20,10 @@ const {
 const {
   spawnSync
 } = require('child_process');
+/**
+ * 
+ */
+Logs = new Mongo.Collection('logs')
 /**
  * 
  */
@@ -33,6 +41,16 @@ log('Loading Setting file: ', files)
 // Meteor.startup(function(){
 //   run('eopkg search text')
 // })
+/**
+ * 
+ */
+const term = pty.spawn('bash', [], {
+  name: 'xterm-color',
+  cols: 80,
+  rows: 30,
+  cwd: process.env.HOME,
+  env: process.env
+});
 /**
  * 
  */
@@ -79,36 +97,82 @@ Meteor.methods({
      *  Test file : '/home/neox/run/log.js' 
      */
     log('--Running command', command)
-    try {
-      var command = run(command)
-      return {
-        log: d,
-        err: null,
-        status: 'Success'
-      }
-    } catch (err) {
-      console.error(err);
-      return {
-        log: 'ERROR: ' + err,
-        err: null,
-        status: 'Success'
-      }
-    }
+    term.write(command + '\r')
+    /**
+     * Run Command: run()
+     */
+    // try {
+    //   var result = run(command)
+    //   log('Result',result.split('\n'))
+    //   var logData = result.split('\n')
+    //   Logs.insert({
+    //     log: logData,
+    //     createdAt: new Date()
+    //   })
+    //   // return {
+    //   //   log: result,
+    //   //   err: null,
+    //   //   status: 'Success'
+    //   // }
+    // } catch (err) {
+    //   console.error(err);
+    //   return {
+    //     log: 'ERROR: ' + err,
+    //     err: null,
+    //     status: 'Success'
+    //   }
+    // }
   }
 })
 /**
- * 
+ *  Run Command
+ * === Child execSync
  */
 function run(command) {
-  log('Running: ',command)
+  log('Running: ', command)
   return execSync(command).toString().trim();
 }
+/** */
+// function termData() {
+  term.on('data', Meteor.bindEnvironment(function (data) {
+    console.log(data);
+    // var logData = data.split('\n')
+    var d = cleanData(data)
+    if(data.includes("neox@neoxnuc ~ $") || data.includes("Command completed") || data.includes('neox@neoxnuc')){
+      return
+    }
+    Logs.insert({
+      log: d,
+      createdAt: new Date()
+    })
+  }));
+// }
+/**
+ * Publish
+ */
+Meteor.publish(null, () => {
+  return Logs.find({},{limit:100})
+})
+/**
+ * 
+ * Clean and Filter Data 
+ * === (For pty.js)
+ * /*
+
+    // term.write('ls\r');
+    // term.resize(100, 40);
+    // term.write('ls /\r');
 
 
+    // WORKING
+    // term.write('eopkg search inkscape\r');
 
-shell.exec('eopkg search inkscape')
-
-if (shell.exec('git commit -am "Auto-commit"').code !== 0) {
-  shell.echo('Error: Git commit failed');
-  shell.exit(1);
+    // term.write('ls /\r')
+ */
+function cleanData(data){
+  // return data.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, " ")
+  var data = data.replace(/otify;/g, "")
+  var data = data.replace(/777;preexec/g, "")
+  // return data = data.replace(/\033\[[0-9;]*m/,"")
+  return stripAnsi(data)
 }
