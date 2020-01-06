47a53b36adce0b258101f7ef0dbd7177e7a51dcc
Zeno R.R. Davatz <zdavatz@gmail.com>
2020-01-06 15:05:50 +0100

    add _meteor add tarang:ssl_ to default install.

--- server/main.js
+++ server/main.js
@@ -68,7 +68,7 @@ Meteor.startup(function () {
   if (settings.ssl) {
 
     SSLProxy({
-      port: 6000, //or 443 (normal port/requires sudo)
+      port: 3100, //or 443 (normal port/requires sudo)
       ssl: {
         key: Assets.getText("key.pem"),
         cert: Assets.getText("cert.pem"),
